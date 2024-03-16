e6250aac6d838022f8c5275695afa60bdaa27d7a
rkavidu222 <123630912+rkavidu222@users.noreply.github.com>
2024-03-16 11:57:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("First web project");
+
+var navLinks = document.getElementById("navLinks");
+var bodyId = document.getElementById("bodyId");
+var htmlId = document.getElementById("htmlId");
+
