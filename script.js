83a6629653a3fcf3841564edef1fa00c9324db3d
rkavidu222 <123630912+rkavidu222@users.noreply.github.com>
2024-03-21 14:47:20 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-console.log("First web project");
+console.log("Javascript is Linked Code Point 2K23 !!!");
 
 var navLinks = document.getElementById("navLinks");
 var bodyId = document.getElementById("bodyId");
 var htmlId = document.getElementById("htmlId");
 
+
+function hideMenu() {
+    bodyId.style.overflowY = "visible";
+    htmlId.style.overflowY = "visible";
+    navLinks.style.right = "-200px";
+}
+function showMenu() {
+    window.scrollTo(0, 0);
+    navLinks.style.right = "0";
+    bodyId.style.overflowY = "hidden";
+    htmlId.style.overflowY = "hidden";
+}
