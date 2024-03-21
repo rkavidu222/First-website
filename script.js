a1618d209e39d3bbc6d7fd3c80d6a468a3beba50
rkavidu222 <123630912+rkavidu222@users.noreply.github.com>
2024-03-21 14:53:12 +0530

    script.js

--- script.js
+++ script.js
@@ -5,14 +5,4 @@ var bodyId = document.getElementById("bodyId");
 var htmlId = document.getElementById("htmlId");
 
 
-function hideMenu() {
-    bodyId.style.overflowY = "visible";
-    htmlId.style.overflowY = "visible";
-    navLinks.style.right = "-200px";
-}
-function showMenu() {
-    window.scrollTo(0, 0);
-    navLinks.style.right = "0";
-    bodyId.style.overflowY = "hidden";
-    htmlId.style.overflowY = "hidden";
-}
+
